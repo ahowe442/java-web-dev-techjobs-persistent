431e5e720569e6cec2089cf9bc7519a952759085
ahowe442 <howe.abigail@gmail.com>
2020-03-03 20:08:12 -0600

    Assignment_4-Part_2-Step_4 - Adds @Entity to skill class.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,8 +1,10 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Skill extends AbstractEntity {
 
     @Size(max = 500, message = "Description too long!")
