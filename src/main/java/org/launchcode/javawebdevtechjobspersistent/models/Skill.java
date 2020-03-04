6c692bed96bf7771570084d123e884c4c372aa59
ahowe442 <howe.abigail@gmail.com>
2020-03-03 20:06:59 -0600

    Assignment_4-Part_2-Step_3 - Adds description to Skill class as well as 500 char max description validation.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,11 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
+
 public class Skill extends AbstractEntity {
+
+    @Size(max = 500, message = "Description too long!")
+    String description;
 
 }
