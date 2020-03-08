a8b67402840b33a5b575c4ed0cdfe2c5ea55ed99
ahowe442 <howe.abigail@gmail.com>
2020-03-08 18:57:26 -0500

    adds getters and setters to Skill class.  This also made the view work.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -8,9 +8,15 @@ import javax.validation.constraints.Size;
 public class Skill extends AbstractEntity {
 
     @Size(max = 255, message = "Description too long!")
-    String description;
+    private String description;
 
     public Skill (){}
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
