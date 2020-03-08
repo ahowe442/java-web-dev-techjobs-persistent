c0a430c4e25278ccf0a0c362ac1b025adb3c3eed
ahowe442 <howe.abigail@gmail.com>
2020-03-08 12:50:54 -0500

    Assignment_Controllers step 3 completed. Skill Controller completed based on EmployerController.  (Unsure about accuracy of this code)

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/SkillController.java
@@ -1,4 +1,44 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("skills")
 public class SkillController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "skills/add";
+        }
+        skillRepository.save(newSkill);
+        return "redirect:";
+    }
+
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(Model model, @PathVariable Integer skillId) {
+
+
+        if (skillId != null) {
+            model.addAttribute("employer", skillRepository.findById(skillId));
+            return "employers/view";
+        } else return "redirect:../";
+    }
+
+
 }
