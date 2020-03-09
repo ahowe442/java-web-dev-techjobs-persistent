5db5814a459c0d9f0546ddacd25d5f97aafa3bc0
ahowe442 <howe.abigail@gmail.com>
2020-03-09 14:48:06 -0500

    Added part 1 & 2 of queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,12 @@
 ## Part 1: Test it with SQL
 
+SELECT * FROM techjobs.job;
+
 ## Part 2: Test it with SQL
+
+SELECT *
+FROM employer
+WHERE location = "St. Louis City"
 
 ## Part 3: Test it with SQL
 
