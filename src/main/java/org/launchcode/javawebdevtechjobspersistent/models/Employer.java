b6535f73fbff339109c158594f148fd7f752a869
ahowe442 <howe.abigail@gmail.com>
2020-03-08 19:31:11 -0500

    adds a jobs to Field to EMployer and updates Job Model to inherit from Abstract Entity.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,9 +1,13 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,6 +15,10 @@ public class Employer extends AbstractEntity {
     @Size(max = 255)
     @NotBlank(message = "Location is required")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<Job>();
 
     public Employer() {}
 
